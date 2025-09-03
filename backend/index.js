f126a4f268b00c5541dde48d5938ad604b9e8f17
Akash Maity <am6030920@gmail.com>
2025-09-04 01:55:30 +0530

    Added backend and Prasarnettt folders

---
+++ backend/index.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const express = require("express");
+const body_parser = require("body-parser");
+const http = require("http");
+const morgan = require ("morgan");
+const cors = require ("cors");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT;
+const BASE_URL = process.env.BASE_URL;
+const BASE_API = process.env.BASE_API;
+const BASE_API_URL= process.env.BASE_API_URL;
+const ALLOWED_ORIGIN = process.env.ALLOWED_ORIGIN;
+const ALLOWED_METHODS  = process.env.ALLOWED_METHODS;
+const ALLOWED_HEADERS  = process.env.ALLOWED_HEADERS;
+
+const app = express();
+
+app.use(cors({
+    origin : ALLOWED_ORIGIN,
+    methods : ALLOWED_METHODS,
+    allowedHeaders : ALLOWED_HEADERS
+}));
+
+app.use (express.json());
+app.use (morgan('dev'));
+const server = http.createServer(app);
+server.listen(PORT,()=>`this project is running on port : ${PORT}\n and the url is : ${BASE_URL}`)
+
+
+
+
+
